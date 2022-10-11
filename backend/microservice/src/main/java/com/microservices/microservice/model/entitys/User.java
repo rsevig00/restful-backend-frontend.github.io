5780e11dba6c4744995087c8b224a91371653c68
rsevig00 <rsevig00@estudiantes.unileon.es>
2022-10-11 11:05:30 +0200

    Sistema de permanencia permanente configurado

--- backend/microservice/src/main/java/com/microservices/microservice/model/entitys/User.java
+++ backend/microservice/src/main/java/com/microservices/microservice/model/entitys/User.java
@@ -13,7 +13,7 @@ public class User
 {  
 	
 @Id
-@GeneratedValue(strategy = GenerationType.AUTO)
+@GeneratedValue(strategy = GenerationType.IDENTITY)
 private Long id;
 @Column(name="name")
 private String name;  
