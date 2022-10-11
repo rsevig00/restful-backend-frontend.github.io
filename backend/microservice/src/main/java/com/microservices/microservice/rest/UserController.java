5c20a096456c0c30df815a1257a33d4922b670ab
Prieto <dpriea01@estudiantes.unileon.es>
2022-10-11 11:40:16 +0200

    Modificado controllador con puerto

--- backend/microservice/src/main/java/com/microservices/microservice/rest/UserController.java
+++ backend/microservice/src/main/java/com/microservices/microservice/rest/UserController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.microservices.microservice.model.entitys.User;
 import com.microservices.microservice.model.entitys.UserRepository;
 @RestController
-@CrossOrigin(origins = "http://localhost:8080")
+@CrossOrigin(origins = "http://localhost:4200")
 public class UserController {
 
     // standard constructors
@@ -20,7 +20,6 @@ public class UserController {
 
     @GetMapping("/users")
     public List<User> getUsers() {
-    	System.out.println(userRepository + " hijo de puta ");
         return (List<User>) userRepository.findAll();
     }
 
