4dc37606fdc262a045550c57117d95c1853b9209
Prieto <dpriea01@estudiantes.unileon.es>
2022-10-14 10:56:26 +0200

    Cambios en h2 para docker

--- backend/microservice/src/main/java/com/microservices/microservice/MicroserviceApplication.java
+++ backend/microservice/src/main/java/com/microservices/microservice/MicroserviceApplication.java
@@ -20,25 +20,4 @@ public class MicroserviceApplication {
 		SpringApplication.run(MicroserviceApplication.class, args);
 		
 	}
-	@Bean
-    CommandLineRunner init(UserRepository userRepository) {
-        return args -> {
-            Stream.of("John", "Julie", "Jennifer", "Helen", "Rachel").forEach(name -> {
-                User user = new User(name, name.toLowerCase() + "@domain.com");
-                userRepository.save(user);
-            });
-            userRepository.findAll().forEach(System.out::println);
-        };
-    }
-	
-	@Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("*").allowedMethods("GET", "POST","PUT", "DELETE");
-            }
-        };
-    }
-	
 }
