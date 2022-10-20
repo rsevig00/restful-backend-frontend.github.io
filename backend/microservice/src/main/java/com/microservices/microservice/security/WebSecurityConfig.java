58fac085778fb08fb940545d3e52baf8ec40fa7f
Prieto <dpriea01@estudiantes.unileon.es>
2022-10-20 12:14:09 +0200

    Corregido error en /api/auth

--- backend/microservice/src/main/java/com/microservices/microservice/security/WebSecurityConfig.java
+++ backend/microservice/src/main/java/com/microservices/microservice/security/WebSecurityConfig.java
@@ -12,7 +12,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 //import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 //import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -64,9 +64,13 @@ public class WebSecurityConfig { // extends WebSecurityConfigurerAdapter {
     return authConfig.getAuthenticationManager();
   }
 
+//  @Bean
+//  public PasswordEncoder passwordEncoder() {
+//    return new BCryptPasswordEncoder();
+//  }
   @Bean
   public PasswordEncoder passwordEncoder() {
-    return new BCryptPasswordEncoder();
+      return NoOpPasswordEncoder.getInstance();
   }
 
 //  @Override
