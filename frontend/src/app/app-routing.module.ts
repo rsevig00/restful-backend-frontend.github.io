1e4618b24262d3da849443b51a082cafe3c18fa4
cdanniel <cfernf07@estudiantes.unileon.es>
2022-10-21 11:00:27 +0200

    rutas privadas

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -3,9 +3,11 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 import { LoginComponent } from './components/login/login.component';
+import { AuthGuard } from './auth-guard';
+import { AuthService } from './services/auth.service';
 
 const routes: Routes = [
-  {path: 'home', component: HomeComponent},
+  {path: 'home', canActivate:[AuthGuard], component: HomeComponent},
   {path: '', component: LoginComponent},
   { path: '**', pathMatch: 'full', 
         component: PageNotFoundComponent },
@@ -13,6 +15,7 @@ const routes: Routes = [
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: [AuthGuard, AuthService]
 })
 export class AppRoutingModule { }
