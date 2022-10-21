2989bf15c1f7289ecdd358aa793a8437d0276175
rsevig00 <rsevig00@estudiantes.unileon.es>
2022-10-21 12:57:59 +0200

    Añadido testing user controller

---
+++ backend/microservice/src/test/java/com/microservices/microservice/rest/UserControllerTest.java
@@ -0,0 +1,49 @@
+package com.microservices.microservice.rest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.microservices.microservice.model.entitys.User;
+
+@SpringBootTest
+public class UserControllerTest {
+
+	UserController uc;
+	
+	@BeforeAll
+	public void setUp() {
+		uc = new UserController();
+	}
+
+	@Test
+	public void emptyDB() {
+		assertEquals(1, uc.getUsers().size(), "DB should have one admin user on creation");
+	}
+	
+	@Test
+	public void addUser() {
+		uc.addUser(new User("Pepe", "pepe@pepe.com", "1234"));
+		assertEquals(2, uc.getUsers().size(), "DB should have one admin user on creation");
+	}
+	
+	@Test
+	public void removeUser() {
+		uc.removeUser(1L);
+		assertEquals(0, uc.getUsers().size(), "DB should have one admin user on creation");
+	}
+	
+	@Test
+	public void modifyUser() {
+		User modifiedUser = new User("Pepe", "pepe@pepe.com", "1234");
+		modifiedUser.setId(1L);
+		uc.modifyUser(modifiedUser);
+		assertEquals(1, uc.getUsers().size(), "DB should have one admin user on creation");
+		assertEquals("Pepe", uc.getUsers().get(1).getName(), "");
+		assertEquals("pepe@pepe.com", uc.getUsers().get(1).getEmail(), "");
+		assertEquals("1234", uc.getUsers().get(1).getPassword(), "");
+	}
+	
+}
