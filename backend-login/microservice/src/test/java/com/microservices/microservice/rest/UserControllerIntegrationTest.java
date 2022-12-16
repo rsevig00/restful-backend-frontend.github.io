6634ef46c27653ebd9fce5071e66a01eac2fcba9
rsevig00 <rsevig00@estudiantes.unileon.es>
2022-12-16 10:55:43 +0100

    Añadido test userControllerIntegrationTest

---
+++ backend-login/microservice/src/test/java/com/microservices/microservice/rest/UserControllerIntegrationTest.java
@@ -0,0 +1,57 @@
+package com.microservices.microservice.rest;
+
+import com.microservices.microservice.model.entitys.UserRepository;
+
+import org.json.JSONObject;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc(addFilters = false)
+public class UserControllerIntegrationTest {
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	@MockBean
+	private UserRepository userRepository;
+
+	@Test
+	public void testGetUser() throws Exception {
+		this.mockMvc.perform(get("http://backend-users:8080/users/users")).andDo(print()).andExpect(status().isOk());
+	}
+
+	@Test
+	public void testPostUser() throws Exception {
+		String jsonString = new JSONObject().put("username", "diego").put("email", "diego@domain.es")
+				.put("password", "11AA##aa").toString();
+		this.mockMvc
+				.perform(post("http://localhost:8082/users/users").content(jsonString)
+						.contentType(MediaType.APPLICATION_JSON))
+				.andDo(print()).andExpect(status().is2xxSuccessful()).andReturn();
+		this.mockMvc.perform(delete("http://localhost:8082/users/users/diego")).andDo(print())
+				.andExpect(status().isOk());
+	}
+
+	@Test
+	public void testModifyDeleteUser() throws Exception {
+		String jsonString = new JSONObject().put("id", 1).put("username", "diego").put("email", "diego@domain.es")
+				.put("password", "11AA##aa").toString();
+		this.mockMvc
+				.perform(post("http://localhost:8082/users/users").content(jsonString)
+						.contentType(MediaType.APPLICATION_JSON))
+				.andDo(print()).andExpect(status().is2xxSuccessful()).andReturn();
+		this.mockMvc.perform(delete("http://localhost:8082/users/users/diego")).andDo(print())
+				.andExpect(status().isOk()); 
+	}
+
+}
